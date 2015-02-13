f521102ac25c06a1fe22b81f57e02ebe05967279
gallochri <chri@gallochri.com>
2015-02-13 19:06:04 +0100

    update Scrippetone

--- Scrippettone.sh
+++ Scrippettone.sh
@@ -7,3 +7,19 @@ mkdir modules
 MODULES_TEMP=$PWD/modules/
 mkdir KERNEL
 KERNEL_PATH=$PWD/KERNEL/
+
+#Clone source
+
+TOOLS_GIT=https://github.com/raspberrypi/tools.git
+
+echo "Clonare il kernel da https://github.com/raspberrypi/linux.git [s/n] (s)?"
+read LINUX_GIT
+if [ "$LINUX_GIT" = "s" ]
+then 
+	LINUX_GIT=https://github.com/raspberrypi/linux.git
+else
+	echo "Inserire URL git da cui clonare il kernel:"
+	read LINUX_GIT
+fi
+	
+echo "$LINUX_GIT"
