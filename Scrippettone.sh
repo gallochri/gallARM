65e4e0b6a1aabeaa4fc9396752b72fbb4d3643aa
gallochri <chri@gallochri.com>
2015-02-17 20:10:13 +0100

    Scrippettone terminate

--- Scrippettone.sh
+++ Scrippettone.sh
@@ -31,21 +31,20 @@ done
 echo ""
 while true; do
 	echo "Scegliere il file .config:"
-	echo "1- scelta A"
-	echo "2- scelta B"
-	echo "3- scelta C"
+	echo "1- Configura manualmente"
+	echo "2- config 3.10.26"
+	echo "3- config 3.18.6"
 	read -n 1 -r
 	case $REPLY in
-		[1]* ) echo "A"; break;;
-		[2]* ) echo "B"; break;;
-		[3]* ) echo "C"; break;;
+		[1]* ) CONFIG=1; break;;
+		[2]* ) CONFIG=2; break;;
+		[3]* ) CONFIG=3; break;;
 		* ) echo -e "\nErrore.Scegliere una opzione";;
 	esac
 done
 
-exit 
-
 ###Esecuzione####
+echo "Cloning...."
 git clone $LINUX_GIT
 git clone $TOOLS_GIT
 
@@ -53,12 +52,26 @@ git clone $TOOLS_GIT
 wget http://xecdesign.com/downloads/linux-qemu/linux-arm.patch
 patch -p1 -d linux/ < linux-arm.patch
 
-#Clean kernel config
+#Kernel config
+echo "Config..."
 cd linux
 make mrproper
+case $CONFIG in
+	[1]* ) make versatile_defconfig ARCH=arm CROSS_COMPILE=${CCPREFIX}; make menuconfig ARCH=arm CROSS_COMPILE=${CCPREFIX}; break;;
+	[2]* ) cp ../kernel_config/config_3.10.26 ${KERNEL_SRC}/.config; make oldconfig ARCH=arm CROSS_COMPILE=${CCPREFIX}; break;;
+	[3]* ) cp ../kernel_config/config_3.18.6 ${KERNEL_SRC}/.config; make oldconfig ARCH=arm CROSS_COMPILE=${CCPREFIX}; break;;
+esac
 
+#Build kernel
+echo "Building..."
+make ARCH=arm CROSS_COMPILE=${CCPREFIX}
+make modules_install ARCH=arm CROSS_COMPILE=${CCPREFIX} INSTALL_MOD_PATH=${MODULES_TEMP}
 
+#Extract kernel image
+echo "Extracting image..."
+cd ../tools/mkimage/ 
+./imagetool-uncompressed.py ${KERNEL_SRC}/arch/arm/boot/zImage
+cp -r ${KERNEL_SRC}/arch/arm/boot/zImage ${KERNEL_PATH}
+cp -r kernel.img ${KERNEL_PATH}
 
-echo ""
-echo "$LINUX_GIT"
-echo "$TOOLS_GIT"
+echo "The End"
